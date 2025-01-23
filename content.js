84a44cbbb7491373b83ca92eabb6218c801c3e3a
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 20:53:35 -0300

    adding the search bar  again to de youtube(saul is better but you have to use the search bar right?)

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 setInterval(() => {
     document.querySelectorAll("img").forEach(img => {
-        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+        if (img.className === "ytdTextInputAssistantIcon"){
+            img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg"
+        }
+        else{
+            img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+        }
+        
     });
 }, 2000);
