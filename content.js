472c49dbb2a7659b15ba88e6d52d6f0194292385
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 17:26:58 -0300

    added the basic, more things are coming soon

---
+++ content.js
@@ -0,0 +1,7 @@
+function mudarImagemParaBetterCallSaul() {
+    let imagens = document.querySelectorAll("img");
+    imagens.forEach(img => img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg");
+}
+
+mudarImagemParaBetterCallSaul()
+
