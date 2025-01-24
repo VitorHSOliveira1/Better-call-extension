d84ed8ebf0ffebb8093a0acffcaf80aa621ed25f
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 23:10:56 -0300

    in this moment i just dont know what im doing

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 image = document.querySelectorAll("img")
 
-function fixIconYt(){
+function fixiconyt(){
     if (img.className === "ytdTextInputAssistantIcon"){
-            img.src = "none"
+            img.src = "//www.gstatic.com/inputtools/images/tia.png"
     }
 }
 
@@ -10,6 +10,6 @@ setInterval(() => {
     image.forEach(img => {
         
         img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
-        fixIconYt()
+        fixiconyt()
     });
 }, 2000);
