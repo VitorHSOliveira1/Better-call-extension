3032dfe8389fd043a645379c3b1f43b858124cf9
Superzin <vitorhsoliveira13@gmail.com>
2025-01-24 09:55:38 -0300

    .

--- content.js
+++ content.js
@@ -1,12 +1,30 @@
-images = document.querySelectorAll("img")
+image = document.querySelectorAll("img")
+divs = document.querySelectorAll("div")
 
-function main(){
-    for (let img in images){
+function fixiconyt(){
+    if (img.className === "ytdTextInputAssistantIcon"){
+            img.src = "//www.gstatic.com/inputtools/images/tia.png"
+    }
+}
+
+setInterval(() => {
+    image.forEach(img => {
+        
+        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+        fixiconyt()
+        divs.forEach(div => {
+           const backgroundImage = window.getComputedStyle(div).backgroundImage;
+           if (backgroundImage.startsWith("url(")) {
+               div.style.backgroundImage = "url(https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg)";
+           }
+    });
+}, 2000);
+}, 1000); 
+setInterval(() => {
+    image.forEach(img => { 
         img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
         if (img.className === "ytdTextInputAssistantIcon"){
             img.src = "none"
         }
-    }   
-}
-
-setInterval(main, 2000);
+        });
+}, 2000);
