8d7d13c59415e9f82a86f23d0b064a0b8963fb57
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 21:55:31 -0300

    now, in teory the extension should work for netflix, hgbo and crunchroll

--- content.js
+++ content.js
@@ -1,15 +1,28 @@
 image = document.querySelectorAll("img")
+divs = document.querySelectorAll("div")
 
-function fixiconyt(){
-    if (img.className === "ytdTextInputAssistantIcon"){
-            img.src = "//www.gstatic.com/inputtools/images/tia.png"
-    }
-}
 
 setInterval(() => {
-    image.forEach(img => {
-        
-        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
-        fixiconyt()
+        divs.forEach(div => {
+           const backgroundImage = window.getComputedStyle(div).backgroundImage;
+
+           if (backgroundImage.startsWith("url(")) {
+               div.style.backgroundImage = "url(https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg)";
+           }
     });
+}, 1000); 
+
+
+
+setInterval(() => {
+    image.forEach(img => { 
+        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+        if (img.className === "ytdTextInputAssistantIcon"){
+            img.src = "none"
+        }
+        });
 }, 2000);
+
+
+
+
