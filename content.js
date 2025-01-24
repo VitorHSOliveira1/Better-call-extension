41781d024979e3af14a16881566f9176186be67b
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 21:14:11 -0300

    i am dumb, now the yt search is right

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
+imagem = document.querySelectorAll("img")
+
 setInterval(() => {
-    document.querySelectorAll("img").forEach(img => {
+    imagem.forEach(img => {
         if (img.className === "ytdTextInputAssistantIcon"){
-            img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg"
+            img.src = "//www.gstatic.com/inputtools/images/tia.png"
         }
-        else{
-            img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
-        }
-        
+        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+ 
     });
 }, 2000);
