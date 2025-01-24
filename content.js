11f855a86971e1c4c1378b2a8886a4099dc995a5
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 21:17:37 -0300

    now its solved fr

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
-imagem = document.querySelectorAll("img")
+image = document.querySelectorAll("img")
+
+function fixiconyt(){
+    if (img.className === "ytdTextInputAssistantIcon"){
+            img.src = "//www.gstatic.com/inputtools/images/tia.png"
+    }
+}
 
 setInterval(() => {
-    imagem.forEach(img => {
-        if (img.className === "ytdTextInputAssistantIcon"){
-            img.src = "//www.gstatic.com/inputtools/images/tia.png"
-        }
+    image.forEach(img => {
+        
         img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
- 
+        fixiconyt()
     });
 }, 2000);
