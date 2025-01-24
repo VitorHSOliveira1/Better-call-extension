822b2de767f1df63413fb38ff540920466c5a0fd
Superzin <vitorhsoliveira13@gmail.com>
2025-01-24 00:04:10 -0300

    i will try to "fix" that thing tomorrow

--- content.js
+++ content.js
@@ -1,24 +1,12 @@
-image = document.querySelectorAll("img")
-divs = document.querySelectorAll("div")
+images = document.querySelectorAll("img")
 
-
-setInterval(() => {
-        divs.forEach(div => {
-           const backgroundImage = window.getComputedStyle(div).backgroundImage;
-
-           if (backgroundImage.startsWith("url(")) {
-               div.style.backgroundImage = "url(https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg)";
-           }
-    });
-}, 1000); 
-
-
-
-setInterval(() => {
-    image.forEach(img => { 
+function main(){
+    for (let img in images){
         img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
         if (img.className === "ytdTextInputAssistantIcon"){
             img.src = "none"
         }
-        });
-}, 2000);
+    }   
+}
+
+setInterval(main, 2000);
