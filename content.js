b4a1326cdd9aa2202f8a75b9d1ada58453a69696
Superzin <vitorhsoliveira13@gmail.com>
2025-01-23 23:07:47 -0300

    i fucked up, so i am fixing

--- content.js
+++ content.js
@@ -1,28 +1,15 @@
 image = document.querySelectorAll("img")
-divs = document.querySelectorAll("div")
 
-
-setInterval(() => {
-        divs.forEach(div => {
-           const backgroundImage = window.getComputedStyle(div).backgroundImage;
-
-           if (backgroundImage.startsWith("url(")) {
-               div.style.backgroundImage = "url(https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg)";
-           }
-    });
-}, 1000); 
-
-
-
-setInterval(() => {
-    image.forEach(img => { 
-        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
-        if (img.className === "ytdTextInputAssistantIcon"){
+function fixIconYt(){
+    if (img.className === "ytdTextInputAssistantIcon"){
             img.src = "none"
-        }
-        });
+    }
+}
+
+setInterval(() => {
+    image.forEach(img => {
+        
+        img.src = "https://media.moddb.com/cache/images/mods/1/56/55177/thumb_620x2000/bandicam_2022-09-05_20-00-55-025.jpg";
+        fixIconYt()
+    });
 }, 2000);
-
-
-
-
